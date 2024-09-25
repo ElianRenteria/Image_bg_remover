a8b85fc6c650ec240f4e9973c9c2852091d38dd6
elianrenteria <elianrenteriadevelopment@gmail.com>
2024-09-25 13:43:15 -0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<main class="container">
+    <h1>Background Remover</h1>
+</main>
+
+<style>
+    h1 {
+        font-weight: 300;
+        text-align: center;
+        margin-top: 1.5rem;
+    }
+</style>
